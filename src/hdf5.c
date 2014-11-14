8d402d79edb9539a8981cd609229e72ca7901205
Matt Talluto <matthew.talluto@uqar.ca>
2014-11-14 09:27:31 -0500

    Basic HDF5 program to verify it is working

---
+++ src/hdf5.c
@@ -0,0 +1,20 @@
+// compile:
+// clang hdf5.c -lhdf5 
+// OR
+// h5cc -o test hdf5.c
+
+#include "hdf5.h"
+
+#define FILE "file.h5"
+
+int main() {
+
+	hid_t       file_id;   /* file identifier */
+	herr_t      status;
+
+	/* Create a new file using default properties. */
+	file_id = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+	/* Terminate access to the file. */
+	status = H5Fclose(file_id); 
+}
