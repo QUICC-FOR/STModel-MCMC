837b6613f9615167e2cdea3d55442d7ad1564fa2
Matt Talluto <mtalluto@gmail.com>
2014-12-16 13:18:43 -0500

    Added basic API for parameters

---
+++ hdr/parameters.hpp
@@ -0,0 +1,27 @@
+#ifndef ST_PARAMS_H
+#define ST_PARAMS_H
+
+#include <map>
+
+namespace Parameters {
+
+class TransitionRates
+{
+	public:
+		long double theta(const char state = 0) const;
+		long double beta(const char state) const;
+		long double phi(const char state) const;
+		long double epsilon() const;
+		
+};
+
+class Parameters
+{
+	public:
+		TransitionRates generate_rates(double env1, double env2, std::map<char> expected);
+};
+
+} // !Parameters namespace
+
+
+#endif
