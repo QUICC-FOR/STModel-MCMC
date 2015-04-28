ea646686c1bd5c340f31449ee562bfceda0a8b0f
Matt Talluto <mtalluto@gmail.com>
2015-04-28 19:29:47 -0400

    Added a SerializationData class to act as an intermediate when reading data from disk

--- hdr/stmtypes.hpp
+++ hdr/stmtypes.hpp
@@ -2,6 +2,7 @@
 #define STM_TYPES_H
 #include <map>
 #include <string>
+#include <vector>
 
 namespace STM
 {
@@ -11,5 +12,37 @@ namespace STM
 	typedef std::map<ParName, ParValue> ParMap;
 	typedef std::map<StateTypes, ParValue> StateMap;
 	typedef std::pair<ParName, ParValue> ParPair;
+
+	class SerializationData
+	{
+		public:
+		SerializationData();
+		
+		template<typename T> std::vector<T> at(std::string n) const;
+		
+		private:
+		std::map<std::string, std::string> sData;
+		
+	};
+	
+	
+	
+	// IMPLEMENTATION
+	template<typename T> std::vector<T> SerializationData::at(std::string n) const
+	{
+		// uses stringstreams to try to convert the raw data (a string) to the requested T type
+		std::vector<T> result;
+		for(const auto & stVal : sData.at(n))
+		{
+			std::istringstream iss;
+			iss.str() = stVal;
+			T val;
+			iss >> val;
+			result.push_back(val);
+		}
+		return result;
+	}
+	
+
 }
 #endif
